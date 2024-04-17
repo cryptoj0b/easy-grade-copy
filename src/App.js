d85df7e72176a2e2263db204a45246fc3192c98d
cryptoj0b <166725203+cryptoj0b@users.noreply.github.com>
2024-04-17 14:54:48 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BrowseForm from './BrowseForm';
 import Footer from './Footer';
 import HomePage from './HomePage';
 import LoginBox from './LoginBox';
-import { BrowserRouter as Router, Route, Link, Routes, useNavigate } from 'react-router-dom';
 import SignupEN from './SignUpEn';
 import LoginPageEn from './LoginPageEn';
 import DashboardEN from './dashboardEN';
 import DashboardAR from './dashboardAR';
-import logo from './easygradepfp.png';
 import HomePageAR from './HomePageAR';
 import LoginPageAR from './LoginPageAR';
 import SignupAR from './SignUpAR';
@@ -15,30 +15,28 @@ import TeacherOrStudent from './TorS';
 import TeacherOrStudentAR from './TorSAR';
 import NewPasswordPageEN from './NewPasswordPageEN';
 import NewPasswordPageAR from './NewPasswordPageAR';
-
-
+import { UserProvider } from './UserContext'; // Ensure this path matches where you created the UserContext file
 
 function App() {
-
   return (
-    <>
-    <Router>
-      <Routes>
-      <Route path="/" element={<DashboardEN/>} />
-      <Route path="/login" element={<LoginPageEn/>} />
-      <Route path="/signup" element={<SignupEN/>} />
-      <Route path='/Home' element= {<HomePage/>} />
-      <Route path='/dashboard-ar' element = {<DashboardAR/>}/>
-      <Route path='/Home-ar' element = {<HomePageAR/>}/>
-      <Route path='/login-ar' element = {<LoginPageAR/>}/>
-      <Route path='/signup-ar' element = {<SignupAR/>}/>
-      <Route path='/teacher-or-student' element = {<TeacherOrStudent/>}/>
-      <Route path='/teacher-or-student-ar' element = {<TeacherOrStudentAR/>}/>
-      <Route path='/create-new-password-en' element = {<NewPasswordPageEN/>}/>
-      <Route path='/create-new-password-ar' element = {<NewPasswordPageAR/>}/>
-      </Routes>
+    <UserProvider> {/* Wrap Router in UserProvider to make user context available globally */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<DashboardEN/>} />
+          <Route path="/login" element={<LoginPageEn/>} />
+          <Route path="/signup" element={<SignupEN/>} />
+          <Route path='/Home' element={<HomePage/>} />
+          <Route path='/dashboard-ar' element={<DashboardAR/>}/>
+          <Route path='/Home-ar' element={<HomePageAR/>}/>
+          <Route path='/login-ar' element={<LoginPageAR/>}/>
+          <Route path='/signup-ar' element={<SignupAR/>}/>
+          <Route path='/teacher-or-student' element={<TeacherOrStudent/>}/>
+          <Route path='/teacher-or-student-ar' element={<TeacherOrStudentAR/>}/>
+          <Route path='/create-new-password-en' element={<NewPasswordPageEN/>}/>
+          <Route path='/create-new-password-ar' element={<NewPasswordPageAR/>}/>
+        </Routes>
       </Router>
-    </>
+    </UserProvider>
   );
 }
 
