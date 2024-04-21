183360798264b7bc00bb60d19b4485672b2784a1
cryptoj0b <166725203+cryptoj0b@users.noreply.github.com>
2024-04-21 06:13:02 -0400

    Update completeNewPassword.js

--- src/completeNewPassword.js
+++ src/completeNewPassword.js
@@ -1,10 +1,1 @@
 
-export const completeNewPassword = (user, newPassword) => {
-  return new Promise((resolve, reject) => {
-    // The user here is expected to be a CognitoUser object
-    user.completeNewPasswordChallenge(newPassword, [], {
-      onSuccess: (result) => resolve(result),
-      onFailure: (err) => reject(err),
-    });
-  });
-};
