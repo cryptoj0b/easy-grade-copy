0c0f39166c8a452d538df129c250283aaa51e725
cryptoj0b <166725203+cryptoj0b@users.noreply.github.com>
2024-04-21 13:36:56 -0400

    Update BrowseForm.js

--- src/BrowseForm.js
+++ src/BrowseForm.js
@@ -13,6 +13,7 @@ export default function BrowseForm(){
     e.preventDefault();
     const file = e.dataTransfer.files[0];
     if (file && file.name.endsWith('.docx')) {
+      // Handle the dropped file here, for example, you can set it to state or perform any desired operation
       console.log('File dropped:', file);
     }
   };
@@ -48,31 +49,32 @@ export default function BrowseForm(){
         <>
         {/* this is for design and drag & drop feature, leave it be...*/}
         <div className='flex justify-center items-center'>
-        <div className="bg-blue-100 w-[80%] sm:w-fit pb-40 pt-20 md:20 px-20 md:10 h-[570px] sm:h-1/2 justify-center items-center rounded-lg " id="dropArea" onDrop={handleDrop}
+        <div className="bg-blue-100 w-[80%] sm:w-fit md:fit pb-40 pt-20 md:20 px-20 md:10 h-[570px] sm:h-1/2 justify-center items-center rounded-lg " id="dropArea" onDrop={handleDrop}
           onDragOver={handleDragOver}
           onDragEnter={handleDragEnter}
           onDragLeave={handleDragLeave}>
           <img src={grayImage} className='object-cover w-96 md:4 justify-center opacity-50 block'></img>
-        <ul className='mt-6 list-disc'>
-          <li className='list-item'>
-            browse & select OR drag & drop your file
-          </li>
-          <li className='list-item'>
-            click on submit file
-          </li>
-          <li className='list-item'>
-            enter the key given by your tutor
-          </li>
-          <li className='list-item'>
-            your file will be done and sent to your tutor
-          </li>
-        </ul>
+          <ul className='mt-6 list-disc' style={{direction: 'rtl'}}>
+  <li className='list-item font-Tajawal' style={{direction: 'rtl'}}>
+    تصفح واختر المستند او ضع ملفك في الصفحة
+  </li>
+  <li className='list-item font-Tajawal' style={{direction: 'rtl'}}>
+    انقر على تقديم الملف
+  </li>
+  <li className='list-item font-Tajawal' style={{direction: 'rtl'}}>
+    أدخل المفتاح الذي أعطاه لك معلمك
+  </li>
+  <li className='list-item font-Tajawal' style={{direction: 'rtl'}}>
+    سيتم تصحيح ملفك وإرساله إلى معلمك
+  </li>
+</ul>
+
         {/* Here's your business, change whatever that isn't part of the design, change the design and bad things are gonna happen. it won't be cinematic. */}
         <form method='post' className='grid mt-6 -ml-10 sm:-m-0' >
           <input type="file" id="file" name="file" accept=".docx" /> {/* btw you can add file types we'd need in the accept parameter there */}
           {/* this key thing majing is for the auto-made key for each assignment made from the tutor, 
           sent to his students, so that when they enter their file it only gets submitted to the tutor */}
-          <input name='key' className='rounded mt-2' value= {key} placeholder=" enter key..." onChange={e => setKey(e.target.value)}/><br/>
+          <input name='key' className='rounded mt-2 font-Tajawal'style={{direction: 'rtl'}} value= {key} placeholder=" أدخل الرمز..." onChange={e => setKey(e.target.value)}/><br/>
           <input type="submit" className='bg-gradient-to-r from-cyan-500 to-blue-500 rounded text-white cursor-pointer' style={{marginTop: "10px"}}/>
         </form>
       </div>
