aa34e6ea18dff87a515eaf58cbe8a41894fb6574
cryptoj0b <166725203+cryptoj0b@users.noreply.github.com>
2024-04-22 06:01:45 -0400

    Update BrowseForm.js

--- src/BrowseForm.js
+++ src/BrowseForm.js
@@ -38,7 +38,7 @@ export default function BrowseForm() {
         setUploading(true);
 
         try {
-            const response = await fetch('https://kxs4wm7nc2.execute-api.eu-north-1.amazonaws.com/dev/upload', {
+            const response = await fetch('https://kxs4wm7nc2.execute-api.eu-north-1.amazonaws.com/dev/ANY', {
                 method: 'POST',
                 body: formData
             });
